aeae5b6032738f4a1f96651788b0d2ce09a6d6f5
Алексей Полоз <polozhev@mail.ru>
2021-12-26 00:29:14 +0300

    Parse time

--- libdev/__init__.py
+++ libdev/__init__.py
@@ -2,7 +2,7 @@
 Initializing the Python package
 """
 
-__version__ = '0.11'
+__version__ = '0.12'
 
 __all__ = (
     '__version__',
--- tests/test_time.py
+++ tests/test_time.py
@@ -1,6 +1,15 @@
-from libdev.time import get_date
+"""
+https://www.epochconverter.com/
+"""
+
+from libdev.time import get_date, parse_time
 
 
 def test_get_date():
     assert get_date(1633427647.018819) == '20211005'
     assert get_date(1633427647.018819, '%d.%m.%Y %H:%M:%S') == '05.10.2021 12:54:07'
+
+def test_parse_time():
+    assert parse_time('7.10.1998 7:00:00') == 907743600
+    assert parse_time('7 октября 1998 года 12:00:00', tz=5) == 907743600
+    assert parse_time('🕒 пн, 20 дек. 2021 г., 00:32:44 MSK') == 1639949564
