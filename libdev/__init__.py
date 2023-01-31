2fe3b8022bcc215f2f65c9230fe9fa6813b6e6d3
Alex Poloz <alexypoloz@gmail.com>
2023-01-31 17:50:50 +0700

    Check public IP

--- libdev/__init__.py
+++ libdev/__init__.py
@@ -2,7 +2,7 @@
 Initializing the Python package
 """
 
-__version__ = '0.48'
+__version__ = '0.49'
 
 __all__ = (
     '__version__',
--- tests/test_dev.py
+++ tests/test_dev.py
@@ -1,17 +1,17 @@
-from libdev.dev import check_private_ip
+from libdev.dev import check_public_ip
 
 
-def test_check_private_ip():
-    assert check_private_ip(None) == None
-    assert check_private_ip('') == None
-    assert check_private_ip('0.0.0.0') == True
-    assert check_private_ip('1.1.1.1') == True
-    assert check_private_ip('10.0.0.0') == False
-    assert check_private_ip('127.0.0.1') == False
-    assert check_private_ip('127.168.0.1') == False
-    assert check_private_ip('172.15.255.255') == True
-    assert check_private_ip('172.17.0.1') == False
-    assert check_private_ip('172.168.0.1') == True
-    assert check_private_ip('192.168.0.0') == False
-    assert check_private_ip('192.168.255.255') == False
-    assert check_private_ip('192.169.255.255') == True
+def test_check_public_ip():
+    assert check_public_ip(None) == None
+    assert check_public_ip('') == None
+    assert check_public_ip('0.0.0.0') == '0.0.0.0'
+    assert check_public_ip('1.1.1.1') == '1.1.1.1'
+    assert check_public_ip('10.0.0.0') == None
+    assert check_public_ip('127.0.0.1') == None
+    assert check_public_ip('127.168.0.1') == None
+    assert check_public_ip('172.15.255.255') == '172.15.255.255'
+    assert check_public_ip('172.17.0.1') == None
+    assert check_public_ip('172.168.0.1') == '172.168.0.1'
+    assert check_public_ip('192.168.0.0') == None
+    assert check_public_ip('192.168.255.255') == None
+    assert check_public_ip('192.169.255.255') == '192.169.255.255'
