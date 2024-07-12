20b05ce097fd070ce33dfb022e772d86b7fe833c
kosyachniy <alexypoloz@gmail.com>
2024-07-12 21:01:05 +0300

    async requests via aiohttp

--- libdev/__init__.py
+++ libdev/__init__.py
@@ -2,6 +2,6 @@
 Initializing the Python package
 """
 
-__version__ = "0.77"
+__version__ = "0.78"
 
 __all__ = ("__version__",)
--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
     python_requires=">=3.7, <4",
     install_requires=[
         "requests",  # Because of conflicts with main repo
+        "aiohttp",
         "python-dotenv==1.0.1",
         "boto3==1.34.136",
         "Pillow==10.3.0",
